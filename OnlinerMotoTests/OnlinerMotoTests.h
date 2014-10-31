e8394a094a7d01a731263bdb06005df3f47d43c2
Igor Karpov <igork.fet@gmail.com>
2014-11-01 00:03:36 +0300

    Storyboard updated to iOS 7. Tabs icons fixed. Search view fixed. Tests upgraded.

--- OnlinerMotoTests/OnlinerMotoTests.h
+++ OnlinerMotoTests/OnlinerMotoTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013 KarpovIV. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface OnlinerMotoTests : SenTestCase
+@interface OnlinerMotoTests : XCTestCase
 
 @end
